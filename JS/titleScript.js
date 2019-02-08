0a4d4cd0f7abd2807b869f284f3b624d5da9e8a7
PACSelig <47426261+PACSelig@users.noreply.github.com>
2019-02-08 00:08:49 -0500

    Initial commit

---
+++ JS/titleScript.js
@@ -0,0 +1,6 @@
+function menuOpen() {
+    var topContent = document.getElementById('titletext');
+    var amount = 10;
+    
+    topContent.style.left = (topContent.style.left + amount) + "%";
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Web_Project
-Created with CodeSandbox
